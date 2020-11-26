3adfe60073fc27c6b04a7ed509d079464c174104
unknown <damian9691@wp.pl>
2020-11-26 20:39:35 +0100

    works

--- src/store/coaches/actions.js
+++ src/store/coaches/actions.js
@@ -1,15 +1,63 @@
 
 export default {
-    registerCoach(context,data){
+   async registerCoach(context,data){
+
+        const coachID  =context.rootGetters.userId;
+
         const coachData = {
-            id: context.rootGetters.userId,
             firstName:data.first,
             lastName:data.last ,
             areas:data.areas,
             description:data.desc,
             hourlyRate:data.rate,
         }
+
+       const response = await fetch(`https://vue-manage.firebaseio.com/coaches/${coachID}.json`,
+       {
+           method:'PUT',
+           body:JSON.stringify(coachData),
+       })
+
+       const responseData = await response.json();
+       if(!response.ok){
+           const error = new Error(responseData.message || 'There was an error')
+           throw error
+       }
         
-        context.commit('registerCoach',coachData)
+
+
+        context.commit('registerCoach',{
+            ...coachData,
+            id:coachID
+        })
+    },
+
+    async loadCoaches(context){
+       
+      const response = await (await fetch(`https://vue-manage.firebaseio.com/coaches.json`)).json();
+      
+      if(!response.ok){
+          //throw error
+      }
+      
+   
+      
+      const coaches = [];
+
+      for( let key in response){
+        
+          const coach = {
+            id:key,
+            firstName:response[key].firstName,
+            lastName:response[key].lastName ,
+            areas:response[key].areas,
+            description:response[key].description,
+            hourlyRate:response[key].hourlyRate,
+          };
+          coaches.push(coach);
+      }
+      
+      context.commit('loadCoach',coaches)
+      
     }
 }
